7b45f8fc2c4612e5a973d323cfd91c10cff6b1aa
Prabhat <prabhatjain15.2001@gmail.com>
2021-11-27 15:18:35 +0530

    Q7

--- Q2 - add two numbers.java
+++ Q2 - add two numbers.java
@@ -8,10 +8,10 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-//https://leetcode.com/problems/add-two-numbers/submissions/
+//https://leetcode.com/problems/add-two-numbers/
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
+        ListNode dummy = new ListNode();
         ListNode temp=dummy;
         int carry = 0;
         while(l1!=null || l2!=null || carry==1)
@@ -29,8 +29,8 @@ class Solution {
             }
             sum+=carry;
             carry=sum/10;
-            ListNode node = new ListNode(sum%10);
-            temp.next=node;
+            temp.next= new ListNode(sum%10);
+            //temp.next=node;
             temp=temp.next;
             
         }
