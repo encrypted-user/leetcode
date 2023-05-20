a8da75663cbd242cbb49b0e6fa789cdb150da014
Prabhat <prabhatjain15.2001@gmail.com>
2023-05-20 18:33:01 +0530

    101. Symmetric Tree

---
+++ 101. Symmetric Tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetricHelper(root.left, root.right);
+    }
+    boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null || right == null)
+            return left == right;
+        if (left.val != right.val)
+            return false;
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+    }
+}
